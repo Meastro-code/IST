039911f87d0fb456a3b9f850b06a92a213cc7d4b
Maestro <codemaestro87@gmail.com>
2023-04-27 13:01:16 +0300

    added counter.js to show state

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Message from './components/message';
 import Sample from './components/sample';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
       <Message />
       <Sample name="Steve" />
+      <Counter />
     </div>
   );
 }
+
 export default App;
 
 
